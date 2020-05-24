d95f0a1913e668e6ff0caa5cb25c66da7f1a6f73
Codejune <kbj9704@gmail.com>
2020-05-24 22:52:35 +0900

    temporary file remove

--- C_PrimeNumber/main.c
+++ C_PrimeNumber/main.c
@@ -11,7 +11,8 @@ void ssu_runtime(struct timeval *begin_t, struct timeval *end_t);
 int main(void)
 {
 	int input;
-	int num, denom;
+	int num; // 소수 판별 대상
+	int denom; // 나눌 수
 	struct timeval begin_t;
 	struct timeval end_t;
 
@@ -23,21 +24,21 @@ int main(void)
 
 		num = denom = 2;
 		gettimeofday(&begin_t, NULL); // 측정 시작
-		for (int i = 0; i < input; i++)
+		for (int i = 0; i < input; i++) // 출력할 소수의 개수 만큼 반복
 			while (1) {
-				if (num % denom == 0)
-					if(num == denom) {
+				if (num % denom == 0) //  나누었을 때 나머지가 0일 경우
+					if (num == denom) { // 소수일 경우 (자신 이전에 나눌 수 있는 수가 없음)
 #ifdef DEBUG
-						printf("%-d ",denom);
+						printf("%-d ", denom); // 소수 출력
 #endif
-						num++;
-						denom=2;
-						break;
-					} else {
-						num++;
-						denom=2;
+						num++; // 다음 수 탐색
+						denom = 2; // denom 초기화
+						break; // 다음 소수 탐색
+					} else {  // 합성수일 경우 ( 자신 이전에 나눌 수 있는 수가 있음)
+						num++; // 다음 수 탐색
+						denom = 2; // denom 초기화
 					}
-				else denom++;
+				else denom++; // 모든 denom 탐색
 			}
 		printf("\nprocessing complete.\n");
 		gettimeofday(&end_t, NULL);
@@ -45,7 +46,7 @@ int main(void)
 	}
 }
 
-void ssu_runtime(struct timeval *begin_t, struct timeval *end_t) // 실행시간 출력 
+void ssu_runtime(struct timeval *begin_t, struct timeval *end_t) // 실행시간 출력
 {
 	end_t->tv_sec -= begin_t->tv_sec;
 
@@ -57,8 +58,3 @@ void ssu_runtime(struct timeval *begin_t, struct timeval *end_t) // 실행시간
 	end_t->tv_usec -= begin_t->tv_usec;
 	printf("Runtime: %ld:%06ld(sec:usec)\n", end_t->tv_sec, end_t->tv_usec);
 }
-
-
-
-
-
